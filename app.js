09e8a33048eddad1e99e1303b574c8b4ccfe35af
Bosco Lacerda <bosco.baterista@gmail.com>
2023-11-03 15:10:37 -0300

    While (Enquanto)

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 alert('Bem-vindo ao jogo do número secreto');
-let numeroSecreto = 2;
+let numeroSecreto = 5;
 console.log('Valor do número secreto: ' + numeroSecreto);
 let nome = prompt('Qual seu nome?');
-let chute = prompt(`${nome}, escolha um número entre 0 e 10:`);
-console.log('O usuário chutou: ' + chute);
+let chute
 
-//se chute fo igual ao número secreto
-if(chute == numeroSecreto){
-    console.log('Resultado da comparação:', chute == numeroSecreto);
-    alert(`Isso aí! Você descobriu o número secreto, ${numeroSecreto}.`);
-} else{
-    if (chute > numeroSecreto){
-        alert(`Ops, você errou! O número secreto é menor que ${chute}.`);
+// enquanto o chute não for igual ao número secreto
+while (chute != numeroSecreto){
+    chute = prompt(`${nome}, escolha um número entre 0 e 10:`);
+    console.log('Chute do usuário: ' + chute);
+    //se chute fo igual ao número secreto
+    if(chute == numeroSecreto){
+        console.log('Resultado da comparação:', chute == numeroSecreto);
+        alert(`Isso aí! Você descobriu o número secreto, ${numeroSecreto}.`);
     } else{
-        alert(`Ops, você errou! O número secreto é maior que ${chute}.`);
+        if (chute > numeroSecreto){
+            alert(`Ops, você errou! O número secreto é menor que ${chute}.`);
+        } else{
+            alert(`Ops, você errou! O número secreto é maior que ${chute}.`);
+        }
     }
 }
