c3c0de4dc1cc5cd40c6295c930f850888ef86d82
Bosco Lacerda <bosco.baterista@gmail.com>
2023-11-03 13:56:42 -0300

    Console.log

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 alert('Bem-vindo ao jogo do número secreto');
-let numeroSecreto = 8;
+let numeroSecreto = 2;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 0 e 10:');
+console.log('O usuário chutou: ' + chute);
 
 //se chute fo igual ao número secreto
 if(chute == numeroSecreto){
+    console.log('Resultado da comparação:', chute == numeroSecreto);
     alert(`Isso aí! Você descobriu o número secreto, ${numeroSecreto}.`);
 } else{
-    alert('Que pena! Você errou o número secreto!')
+    console.log('Valor do número secreto:', numeroSecreto);
+    alert('Que pena! Você errou, o número secreto era: ' + numeroSecreto);
 }
