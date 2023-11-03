72097121a2a01b48523f1051b951370280973442
Bosco Lacerda <bosco.baterista@gmail.com>
2023-11-03 12:26:29 -0300

    Concatenação

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 alert('Bem-vindo ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 8;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 0 e 10:');
 
 //se chute fo igual ao número secreto
 if(chute == numeroSecreto){
-    alert('Isso aí! Você descobriu o número secreto! (5)');
+    alert(`Isso aí! Você descobriu o número secreto, ${numeroSecreto}.`);
 } else{
     alert('Que pena! Você errou o número secreto!')
 }
